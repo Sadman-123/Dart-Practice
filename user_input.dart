a61e02d8158fb5e047204a58850d62f03641c76e
Sadman-123 <19201103123@cse.bubt.edu.bd>
2024-06-25 15:38:52 +0600

    lil bit fun with future

--- user_input.dart
+++ user_input.dart
@@ -1,10 +1,10 @@
 import 'dart:io';
-void main()
-{
+
+void main() async {
   print("Enter a number: ");
   int number = int.parse(stdin.readLineSync() as String);
-  for(int i=1;i<=number;i++)
-    {
-      print(i);
-    }
+  for (int i = 1; i <= number; i++) {
+    print(i);
+    await Future.delayed(Duration(seconds: 1));
+  }
 }
