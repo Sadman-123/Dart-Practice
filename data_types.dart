2612a177b5bfd86a899cd4ef68cea3cb04b52085
Sadman-123 <19201103123@cse.bubt.edu.bd>
2024-06-25 09:39:17 +0600

    Data Types

---
+++ data_types.dart
@@ -0,0 +1,16 @@
+void main()
+{
+  //Data types
+  int x=12;
+  String xx="Hello";
+  double y=12.5;
+  bool z=true;
+  List<int> list=[1,2,3,4,5];
+  Set<int> set={1,2,3,4,5};
+  print(x);
+  print(xx);
+  print(y);
+  print(z);
+  print(list);
+  print(set);
+}
