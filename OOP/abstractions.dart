733bc2f2cdcd28ca3dd05039e2aafc44d49da67a
Sadman-123 <19201103123@cse.bubt.edu.bd>
2024-06-25 13:14:00 +0600

    Abstractions

---
+++ OOP/abstractions.dart
@@ -0,0 +1,3 @@
+abstract class Abstractions{
+  void speak();
+}
--- OOP/objects.dart
+++ OOP/objects.dart
@@ -1,11 +1,19 @@
 import 'classes.dart';
 import 'constructors.dart';
 import 'constructors_parameter.dart';
-
+import 'abstractions.dart';
+class Objects implements Abstractions{
+  @override
+  void speak() {
+    print("Nice to Meet you");
+  }
+}
 void main() {
   Classes cls = new Classes();
   print(cls.name);
   print(cls.n);
   Constructors ctr = new Constructors();
   ConstructorsParameter obj = new ConstructorsParameter("Sadman");
+  Objects lol=new Objects();
+  lol.speak();
 }
