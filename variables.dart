739f977c35c88d27fa27b9867d77e02703590649
Sadman-123 <19201103123@cse.bubt.edu.bd>
2024-06-25 10:39:10 +0600

    Variables in dart

---
+++ variables.dart
@@ -0,0 +1,11 @@
+void main()
+{
+  List<String>arr=["Sadman","Sakib","Mahee"];
+  print(arr);
+  var arr2=["my","name","is","shadman"];
+  print(arr2);
+  const int age=23;
+  // age=34;
+  final int lol=3;
+  // lol=4;
+}
